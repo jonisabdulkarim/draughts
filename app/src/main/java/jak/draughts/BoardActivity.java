f6f66b354d7b25b27462b827eb523fa226bf2398
Jonis <jonis.abdulkarim@protonmail.com>
2020-03-08 22:56:44 +0000

    Refactored XML for main menu, coded debug button

--- app/src/main/java/jak/draughts/BoardActivity.java
+++ app/src/main/java/jak/draughts/BoardActivity.java
@@ -2,6 +2,7 @@ package jak.draughts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class BoardActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class BoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        Intent intent = getIntent();
+        char mode = intent.getCharExtra(MainMenuActivity.EXTRA_CHAR, 'X');
     }
 }
--- app/src/main/java/jak/draughts/MainMenuActivity.java
+++ app/src/main/java/jak/draughts/MainMenuActivity.java
@@ -2,13 +2,23 @@ package jak.draughts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
+    public static final String EXTRA_CHAR = "gameMode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
+
+    }
+
+    public void debugBoardActivity(View view) {
+        Intent intent = new Intent(this, BoardActivity.class);
+        intent.putExtra(EXTRA_CHAR, 'D');
+        startActivity(intent);
     }
 }
