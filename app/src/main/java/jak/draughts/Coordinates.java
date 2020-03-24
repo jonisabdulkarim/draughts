4035cd360bf7c43288e59ef72d092d657eae369d
Jonis <jonis.abdulkarim@protonmail.com>
2020-03-24 06:25:24 +0000

    Code Coordinates class

--- app/src/main/java/jak/draughts/Coordinates.java
+++ app/src/main/java/jak/draughts/Coordinates.java
@@ -1,4 +1,32 @@
 package jak.draughts;
 
 public class Coordinates {
+    private int x;
+    private int y;
+
+    public Coordinates(int position) {
+        this.x = position / 8;
+        this.y = position % 8;
+    }
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
