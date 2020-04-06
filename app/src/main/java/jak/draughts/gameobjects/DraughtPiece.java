684d06013d6343dcfe75605bed42779e4d7a4e2b
Jonis <jonis.abdulkarim@protonmail.com>
2020-04-06 12:07:02 +0100

    Code Piece and DraughtPiece

--- app/src/main/java/jak/draughts/gameobjects/DraughtPiece.java
+++ app/src/main/java/jak/draughts/gameobjects/DraughtPiece.java
@@ -1,4 +1,27 @@
 package jak.draughts.gameobjects;
 
-public class DraughtPiece {
+public class DraughtPiece extends Piece {
+    private boolean isRed;
+    private boolean isKing;
+
+    public DraughtPiece(boolean isRed, boolean isKing) {
+        this.isRed = isRed;
+        this.isKing = isKing;
+    }
+
+    public boolean isRed() {
+        return isRed;
+    }
+
+    public void setRed(boolean red) {
+        isRed = red;
+    }
+
+    public boolean isKing() {
+        return isKing;
+    }
+
+    public void setKing(boolean king) {
+        isKing = king;
+    }
 }
--- app/src/main/java/jak/draughts/gameobjects/Piece.java
+++ app/src/main/java/jak/draughts/gameobjects/Piece.java
@@ -1,4 +1,15 @@
 package jak.draughts.gameobjects;
 
+import jak.draughts.Coordinates;
+
 abstract class Piece {
+    private Coordinates coordinates;
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
