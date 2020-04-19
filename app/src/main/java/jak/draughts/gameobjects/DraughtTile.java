3215101eb6edfac6331eeec2467b31b01f7d17e1
Jonis <jonis.abdulkarim@protonmail.com>
2020-04-19 10:32:07 +0100

    Code DraughtTile and Tile, update Gradle

--- app/src/main/java/jak/draughts/gameobjects/DraughtTile.java
+++ app/src/main/java/jak/draughts/gameobjects/DraughtTile.java
@@ -1,4 +1,30 @@
 package jak.draughts.gameobjects;
 
-public class DraughtTile {
+public class DraughtTile extends Tile {
+    DraughtTile(DraughtPiece piece) {
+        setPiece(piece);
+    }
+
+    @Override
+    public DraughtPiece getPiece() {
+        return (DraughtPiece) super.piece;
+    }
+
+    @Override
+    public int getType() {
+        if (isEmpty()) {
+            return 0;
+        }
+
+        DraughtPiece piece = getPiece();
+        if (piece.isRed() && !piece.isKing()) {
+            return 1;
+        } else if (piece.isRed() && piece.isKing()) {
+            return 2;
+        } else if (!piece.isRed() && !piece.isKing()) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 }
--- app/src/main/java/jak/draughts/gameobjects/Tile.java
+++ app/src/main/java/jak/draughts/gameobjects/Tile.java
@@ -1,4 +1,21 @@
 package jak.draughts.gameobjects;
 
 abstract class Tile {
+    protected Piece piece;
+
+    public boolean isEmpty() {
+        return piece == null;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public void removePiece() {
+        this.piece = null;
+    }
+
+    public abstract Piece getPiece();
+
+    public abstract int getType();
 }
